a05a051a0943fb9c4f15b6a1bd31a06184075dba
tycarac <>
2020-06-14 20:12:11 +1000

    Sync install packages script with master

--- install-packages.cmd
+++ install-packages.cmd
@@ -1,18 +1,26 @@
 @echo off
 setlocal
-REM del /s/q *.bak >nul 2>&1
+del /s/q *.bak >nul 2>&1
 
 set logfile=install-python-packages.log
 del /q %logfile% >nul 2>&1
+
+
+REM ___________________________________________________________________________
+REM Output Python version outside virtual environment
+python.exe --version | tee.exe -a %logfile%
 
 REM ___________________________________________________________________________
 REM Create virtural environment
 if not exist ".\.venv" (
 echo Creating virtual environment ...
-python -m venv ".\.venv"
+python -m venv ".\.venv" | tee.exe -a %logfile%
 )
 echo Activating virtual environment ...
-call .venv\Scripts\activate
+call .venv\Scripts\activate | tee.exe -a %logfile%
+
+REM Output Python version inside virtual environment
+python.exe --version | tee.exe -a %logfile%
 
 REM ___________________________________________________________________________
 REM List Python packages
@@ -44,7 +52,11 @@ echo %cmd% | tee.exe -a %logfile%
 
 REM ___________________________________________________________________________
 REM Install/Update packeages from requirements
-if not exist "requirements.txt" goto :eof
+if not exist "requirements.txt" (
+echo. | tee.exe -a %logfile%
+echo "requirements.txt" not found | tee.exe -a %logfile%
+goto :eof
+)
 
 REM Update using requirements.txt file
 set cmd=python -m pip install --no-color --compile --upgrade-strategy eager --upgrade --requirement requirements.txt
@@ -52,7 +64,11 @@ echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
 %cmd% | tee.exe -a %logfile%
 
-REM List Python updated packages
+REM Output Python version inside virtual environment
+echo. | tee.exe -a %logfile%
+python.exe --version | tee.exe -a %logfile%
+
+REM List Python (updated) packages
 set cmd=python -m pip list --no-color
 echo. | tee.exe -a %logfile%
 echo %cmd% | tee.exe -a %logfile%
